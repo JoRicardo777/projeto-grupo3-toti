4193d035b0dbdc14785d118a9378697192f53de4
Ricardo <joanrimalaleal@gmail.com>
2023-11-22 21:37:37 -0300

    Removida pasta innecesaria

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
 
 mongoose 
 .connect(
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apirestgrupo3.joip7bg.mongodb.net/bancodaapi?retryWrites=true&w=majority`
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster1.puqn3zq.mongodb.net/?retryWrites=true&w=majority`
     )
     .then(() => {
         console.log('Conectamos ao MongoDB!')
         app.listen(3000)
 
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err)) 
